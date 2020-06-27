adc70af7866aae74472da42645f00e35694174fd
Col <colw@users.noreply.github.com>
2020-06-27 19:55:10 +0200

    Convert to parcel for browser build (#7)
    * Checking parcel out
    
    * First pass at parcel for local dev.
    
    * Remove old dependency
    
    * Remove debug data
    
    * Change build arguments
    
    * Remove scope hoisting from build process
    
    * Leave react import at parent js file
    
    * Revert "Leave react import at parent js file"
    
    af706611ae3d62d52ccbaf0269fe28cd0f8451b5
    
    * Parcel working with a custom server for the proxy
    
    * Remove favicon from glitch
    
    * Remove unused proxy config file
    
    * Remove axios dependency
    
    * Updated readme

--- src/tools/fetch.js
+++ src/tools/fetch.js
@@ -1,13 +1,11 @@
-import glucose from "../../data/glucose.json";
-import glucoseValues from "../../data/glucoseValues.json";
-let debug = false;
+let host = "";
+if (process.env.NODE_ENV === "development") {
+  host = process.env.DEV_API_HOST;
+}
 
 export async function fetchSugar() {
-  if (debug) {
-    return glucose;
-  }
   try {
-    const response = await window.fetch("/.netlify/functions/latest");
+    const response = await window.fetch(`${host}/.netlify/functions/latest`);
     const data = await response.json();
     return data.glucose;
   } catch (e) {
@@ -16,11 +14,8 @@ export async function fetchSugar() {
 }
 
 export async function fetchLast24() {
-  if (debug) {
-    return glucoseValues;
-  }
   try {
-    const response = await window.fetch("/.netlify/functions/last24");
+    const response = await window.fetch(`${host}/.netlify/functions/last24`);
     const data = await response.json();
     return data.glucoseValues;
   } catch (e) {
