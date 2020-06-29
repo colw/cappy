466b5c282fe91c1e3541a5da57ba72a132b23b79
colw <colw@outlook.com>
2020-06-29 18:37:45 +0200

    Fix: Remove localhost Graphql endpoint

--- src/app.js
+++ src/app.js
@@ -5,8 +5,14 @@ import regeneratorRuntime from "regenerator-runtime";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
 
+let host = "";
+let uri = `${window.location.origin}/.netlify/functions/graphql`;
+if (process.env.NODE_ENV === "development") {
+  uri = `${process.env.DEV_API_HOST}/graphql`;
+}
+
 const client = new ApolloClient({
-  uri: "http://localhost:9000/graphql",
+  uri,
 });
 
 function App() {
