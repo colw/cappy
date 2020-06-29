1392be38558502e1732100b858a2ec8462442ef7
colw <colw@outlook.com>
2020-06-29 17:59:33 +0200

    Use Apollo GraphQL client to fetch values

--- src/app.js
+++ src/app.js
@@ -2,5 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./components/Main";
 import regeneratorRuntime from "regenerator-runtime";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
-ReactDOM.render(<Main />, document.getElementById("main"));
+const client = new ApolloClient({
+  uri: "http://localhost:9000/graphql",
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Main />
+    </ApolloProvider>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("main"));
--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,29 @@ import SugarValue from "./SugarValue";
 import DailyGraph from "./DailyGraph";
 import DailyPie from "./DailyPie";
 
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const LATEST_GLUCOSE_VALUE = gql`
+  {
+    entry {
+      sgv
+      dateString
+      direction
+    }
+  }
+`;
+
+const LATEST_GLUCOSE_VALUES = gql`
+  {
+    entries {
+      sgv
+      dateString
+      direction
+    }
+  }
+`;
+
 function calcAverage(newData) {
   const newAverage =
     newData.reduce((sum, current) => sum + current.sgv, 0) / newData.length;
@@ -36,25 +59,32 @@ function calcTimeInRange(newData) {
 }
 
 const Main = function () {
-  const [glucose, setGlucose] = useState({ sgv: 0 });
-  const [glucoseValues, setGlucoseValues] = useState([{ sgv: 0 }]);
+  const {
+    loading: entryLoading,
+    error: entryError,
+    data: glucoseValue,
+  } = useQuery(LATEST_GLUCOSE_VALUE, {
+    pollInterval: 2.5 * 60 * 1000,
+  });
 
-  useIntervalWithInitial(async () => {
-    const data = await fetchSugar();
-    setGlucose(data);
-  }, 2 * 60 * 1000);
+  const {
+    loading: entriesLoading,
+    error: entriesError,
+    data: glucoseValues,
+  } = useQuery(LATEST_GLUCOSE_VALUES, {
+    pollInterval: 2.5 * 60 * 1000,
+  });
 
-  useIntervalWithInitial(async () => {
-    const data = await fetchLast24();
-    setGlucoseValues(data);
-  }, 2 * 60 * 1000);
+  if (entryLoading || entriesLoading) {
+    return <div>Fetching current glucose…</div>;
+  }
 
   return (
     <div>
-      <SugarValue {...glucose} />
-      <DailyGraph data={glucoseValues} />
-      <div>Average: {calcAverage(glucoseValues).toFixed(0)}</div>
-      <DailyPie data={calcTimeInRange(glucoseValues)} />
+      <SugarValue {...glucoseValue.entry} />
+      <DailyGraph data={glucoseValues.entries} />
+      <div>Average: {calcAverage(glucoseValues.entries).toFixed(0)}</div>
+      <DailyPie data={calcTimeInRange(glucoseValues.entries)} />
     </div>
   );
 };
