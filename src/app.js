8ac636ddcfe5dd601caa8135f9d1c04edcd34d55
colw <colw@outlook.com>
2020-06-30 12:34:16 +0200

    Translate language button

--- src/app.js
+++ src/app.js
@@ -1,9 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Main from "./components/Main";
 import regeneratorRuntime from "regenerator-runtime";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+
+import Main from "./components/Main";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "en",
+  supportedLngs: ["en", "de"],
+  resources: {
+    en: {
+      translation: {
+        average: "Average",
+        languageChange: "Switch to English",
+      },
+    },
+    de: {
+      translation: {
+        average: "Durchschnitt",
+        languageChange: "Zu Deutsch wechseln",
+      },
+    },
+  },
+});
 
 let host = "";
 let uri = `${window.location.origin}/.netlify/functions/graphql`;
@@ -18,7 +41,9 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Main />
+      <I18nextProvider i18n={i18next}>
+        <Main />
+      </I18nextProvider>
     </ApolloProvider>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,47 @@
 import React, { useState } from "react";
 import { useIntervalWithInitial } from "../tools/hooks";
 import { fetchSugar, fetchLast24 } from "../tools/fetch";
+import { useQuery } from "@apollo/react-hooks";
+import { useTranslation } from "react-i18next";
 
 import SugarValue from "./SugarValue";
 import DailyGraph from "./DailyGraph";
 import DailyPie from "./DailyPie";
 
-import { useQuery } from "@apollo/react-hooks";
 import queries from "../graphql/queries";
 import { calcAverage, calcTimeInRange } from "../tools/calculations";
+
+import "./Main.css";
+
+function LanguagewSwitcher() {
+  const [t, intl] = useTranslation();
+
+  let otherLangaugeChoice, languageCode;
+  if (intl.language === "en") {
+    otherLangaugeChoice = "Deutsch";
+    languageCode = "de";
+  } else {
+    otherLangaugeChoice = "English";
+    languageCode = "en";
+  }
+
+  return (
+    <div className="language-switcher">
+      <button onClick={() => intl.changeLanguage(languageCode)}>
+        {t("languageChange")}
+      </button>
+    </div>
+  );
+}
+
+function HeaderComponent() {
+  return (
+    <div className="header-component">
+      <h1>Glucose</h1>
+      <LanguagewSwitcher />
+    </div>
+  );
+}
 
 const Main = function () {
   const {
@@ -27,15 +60,20 @@ const Main = function () {
     pollInterval: 2.5 * 60 * 1000,
   });
 
+  const { t } = useTranslation();
+
   if (entryLoading || entriesLoading) {
     return <div>Fetching current glucose…</div>;
   }
 
   return (
     <div>
+      <HeaderComponent />
       <SugarValue {...glucoseValue.entry} />
       <DailyGraph data={glucoseValues.entries} />
-      <div>Average: {calcAverage(glucoseValues.entries).toFixed(0)}</div>
+      <div>
+        {t("average")}: {calcAverage(glucoseValues.entries).toFixed(0)}
+      </div>
       <DailyPie data={calcTimeInRange(glucoseValues.entries)} />
     </div>
   );
