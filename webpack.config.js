4ac5d571236661976f3f3486bc702b7c52d69adf
colw <colw@outlook.com>
2020-07-02 14:40:14 +0200

    test

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require("path");
 const dotenv = require("dotenv");
 const env = dotenv.config().parsed;
 
+console.log("env", env);
 const envKeys = Object.keys(env).reduce((prev, next) => {
   prev[`process.env.${next}`] = JSON.stringify(env[next]);
   return prev;
